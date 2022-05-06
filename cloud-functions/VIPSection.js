037502e72eb1cd3847ceb37c8d8d0af56ea560cc
HeatherH <hherries@yahoo.com>
2022-05-06 11:10:03 -0600

    Updated cloud function password area

--- cloud-functions/VIPSection.js
+++ cloud-functions/VIPSection.js
@@ -1,6 +1,24 @@
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to the VIP Section!"
-  });
+  const secretContent = `
+  <h3>Welcome to the VIP Section!</h3>
+  <p>Here we give you proprietary details and specials on your next adventure.</p>
+  `;
+  let body;
+
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+
+  if (body.password == "javascript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent
+    });
+  } else {
+    callback(null, {
+      statusCode: 401
+    });
+  }
 };
